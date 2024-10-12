4b4ddd6c60993bd99db61975457e634d5a57dcd4
Toyosee <aminatusman002@gmail.com>
2024-10-12 18:09:07 -0500

    worked on a simple dashboard implemetation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Inspector from './components/Inspector';
 import Login from './Authentication/Login';
 import Signup from './Authentication/Signup';
 import ForgotPassword from './Authentication/ForgotPassword'; // Placeholder for future
 import Dashboard from './pages/Dashboard';
-import StatsDashboard from './pages/StatsDashboard';
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
