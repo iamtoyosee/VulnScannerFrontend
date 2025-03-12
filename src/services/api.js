71f63d7b4479b677817258f13159c95abf56b7c7
Toyosee <aminatusman002@gmail.com>
2025-03-12 17:04:28 -0500

    missed ya

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:5000'; // Replace with your Flask API URL if hosted elsewhere
+const API_BASE_URL = 'http://98.81.199.217'; // Replace with your Flask API URL if hosted elsewhere
 
 // User registration
 export const registerUser = async (userData) => {
